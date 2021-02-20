52061c4a07f3f8b5bbc7a5bd9b0781c1249522c0
Jordan Maier <jimaier89@gmail.com>
2021-02-19 19:54:36 -0800

    started styling background elements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+
+//social icons
+import {AiFillFacebook} from 'react-icons/ai';
+import {FaPinterest} from 'react-icons/fa';
+import {AiOutlineInstagram} from 'react-icons/ai';
+
+//images
+import stars from './images/bg-stars.svg';
+import hills from './images/pattern-hills.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <div className="title">
+        <h3>WE'RE LAUNCHING SOON</h3>
+      </div>
+      <div className="cards-container">
+        {/* <Days />
+        <Hours />
+        <Minutes />
+        <Seconds /> */}
+      </div>
+      <div className="links">
+        <AiFillFacebook className="icon"/>
+        <FaPinterest className="icon" />
+        <AiOutlineInstagram className="icon" />
+      </div>
+      <div className="background-container">
+        <img className="stars" src={stars} />
+        <img className="hills" src={hills}/>
+      </div>
+    </main>
+  )
 }
 
 export default App;
